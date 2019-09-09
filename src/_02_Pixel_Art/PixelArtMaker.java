b387a6658f3193efba51b5167be376209d7e7a55
League Student <league@iMac-12.attlocal.net>
2019-09-08 17:26:09 -0700

    Working on saving the image in pixel art maker
    asdf

--- src/_02_Pixel_Art/PixelArtMaker.java
+++ src/_02_Pixel_Art/PixelArtMaker.java
@@ -1,15 +1,30 @@
 package _02_Pixel_Art;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class PixelArtMaker implements MouseListener{
+
+public class PixelArtMaker implements MouseListener, ActionListener, Serializable{
+	private static final String DATA_FILE = "src/_02_Pixel_Art/saved.dat";
+	
 	private JFrame window;
 	private GridInputPanel gip;
 	private GridPanel gp;
+	private JButton saveImg;
+	private JButton loadImg;
 	ColorSelectionPanel csp;
 	
 	public void start() {
@@ -27,15 +42,28 @@ public class PixelArtMaker implements MouseListener{
 	public void submitGridData(int w, int h, int r, int c) {
 		gp = new GridPanel(w, h, r, c);
 		csp = new ColorSelectionPanel();
+		saveImg = new JButton();
+		loadImg = new JButton();
 		window.remove(gip);
 		window.add(gp);
 		window.add(csp);
+		
+		window.add(saveImg);
+		saveImg.setLabel("Save Image");
+		saveImg.setLocation(w -100 , h + 100);
+		saveImg.addActionListener(this);
+		
+		window.add(loadImg);
+		loadImg.setLabel("Load Image");
+		loadImg.setLocation(w - 50, h + 50);
+		loadImg.addActionListener(this);
+		
 		gp.repaint();
 		gp.addMouseListener(this);
 		window.pack();
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		new PixelArtMaker().start();
 	}
 
@@ -61,5 +89,41 @@ public class PixelArtMaker implements MouseListener{
 
 	@Override
 	public void mouseExited(MouseEvent e) {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == saveImg) {
+			save(new GridPanel(gp.getHeight(), gp.getWidth(), gp.getX(), gp.getY(), gp.pixel));
+		System.out.println("reached");
+		}
+		
+		if (e.getSource() == loadImg) {
+			gp  = load();
+			System.out.println(load());
+			window.pack();
+		}
+		
+	}
+	private static void save(GridPanel gridPanel) {
+		try (FileOutputStream fos = new FileOutputStream(new File(DATA_FILE)); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+			oos.writeObject(gridPanel);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static GridPanel load() {
+		try (FileInputStream fis = new FileInputStream(new File(DATA_FILE)); ObjectInputStream ois = new ObjectInputStream(fis)) {
+			return (GridPanel) ois.readObject();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		} catch (ClassNotFoundException e) {
+			// This can occur if the object we read from the file is not
+			// an instance of any recognized class
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
